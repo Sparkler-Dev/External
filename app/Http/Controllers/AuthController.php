b6b005d675cb1919f254086a22cfdf601f584d5b
thephotogenicbug <navinkumarachari@gmail.com>
2023-05-10 17:16:23 +0530

    Create Signup API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -15,10 +18,18 @@ class AuthController extends Controller
         ]);;
     }
 
-    public function register(){
-        return response()->json([
-            'message'=>'Register Request'
-        ]);
+    public function register(StoreUserRequest $request){
+       $request->validated($request->all());
+       $user = User::create([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=> Hash::make($request->password)
+       ]);
+
+       return $this->success([
+         'user'=>$user,
+         'token'=>$user->createToken('API Token of'. $user->name)->plainTextToken
+       ]);
     }
 
      public function logout(){
