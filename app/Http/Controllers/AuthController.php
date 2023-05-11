19feb86502ad919465e3d4d326f1031c09d4d14a
thephotogenicbug <navinkumarachari@gmail.com>
2023-05-11 12:40:19 +0530

    Create Login Function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(){
-       return response()->json([
-            'message'=>'Login Request'
-        ]);;
+     public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->only(['email', 'password']));
+
+        if(!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Credentials do not match', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token')->plainTextToken
+        ]);
     }
 
     public function register(StoreUserRequest $request){
@@ -32,9 +44,12 @@ class AuthController extends Controller
        ]);
     }
 
-     public function logout(){
-        return response()->json([
-            'message'=>'Logout Request'
+    public function logout()
+    {
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'message' => 'You have succesfully been logged out and your token has been removed'
         ]);
     }
 }
